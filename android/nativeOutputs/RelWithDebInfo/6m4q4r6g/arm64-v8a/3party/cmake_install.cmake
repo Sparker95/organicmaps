30ba788af54f824c67eaabf6198c91a7d1bfe3c6
Saveliy Tronza <sparker_95@mail.ru>
2023-12-30 16:34:53 +0100

    Merge remote-tracking branch 'upstream/master'

---
+++ android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/cmake_install.cmake
@@ -0,0 +1,134 @@
+# Install script for directory: D:/Projects Git/organicmaps/3party
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/omim")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "RelWithDebInfo")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "C:/Users/spark/AppData/Local/Android/Sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-objdump.exe")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/expat/expat/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/jansson/jansson/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/gflags/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/pugixml/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/protobuf/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/agg/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/bsdiff-courgette/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/freetype/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/icu/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/harfbuzz/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/liboauthcpp/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/minizip/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/open-location-code/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/opening_hours/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/sdf_image/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/stb_image/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/succinct/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/arm64-v8a/3party/vulkan_wrapper/cmake_install.cmake")
+endif()
+
---
+++ android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/cmake_install.cmake
@@ -0,0 +1,174 @@
+# Install script for directory: D:/Projects Git/organicmaps
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/omim")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "RelWithDebInfo")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "C:/Users/spark/AppData/Local/Android/Sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-objdump.exe")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/3party/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/base/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/coding/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/descriptions/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/drape/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/drape_frontend/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/editor/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/ge0/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/generator/mwm_diff/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/geometry/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/indexer/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/kml/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/map/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/cppjansson/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/platform/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/routing/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/routing_common/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/search/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/shaders/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/storage/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/tracking/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/traffic/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/transit/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/android/jni/cmake_install.cmake")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "D:/Projects Git/organicmaps/android/nativeOutputs/RelWithDebInfo/6m4q4r6g/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
